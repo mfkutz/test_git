cd95509cbadf93f2b840cb4820a96143907ee511
Martin <mfkutz@gmail.com>
2025-02-02 10:10:41 -0300

    adding text 1

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+        <p>
+        other new text here for test
+        </p>
       </p>
     </>
   )
